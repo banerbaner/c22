a4a4be2550c69d7d4b81aa90dcb3255f1ff49dae
Rehan Siddiqui <rehanbaner@gmail.com>
2020-05-05 19:20:26 +0530

    message

--- particle.js
+++ particle.js
@@ -1,17 +1,17 @@
 class Particle {
-    constructor(x,y,r) {
-      var options = {
-          friction:0,
-          restitution: 0.4
-      }
-      this.body = Bodies.circle(x,y,r,options);
-      this.r = r; 
-      World.add(world, this.body);
+  constructor(x, y, r) {
+    var options = {
+      friction: 0,
+      restitution: 0.4
     }
-    display(){
-      var pos =this.body.position;
-      ellipseMode(CENTER);
-      fill("green");
+    this.body = Bodies.circle(x, y, r, options);
+    this.r = r;
+    World.add(world, this.body);
+  }
+  display() {
+    var pos = this.body.position;
+    ellipseMode(CENTER);
+    fill("green");
     ellipse(pos.x, pos.y, this.r);
-    }
-  };
+  }
+};
--- sketch.js
+++ sketch.js
@@ -5,67 +5,58 @@
 var Engine = Matter.Engine,
     World = Matter.World,
     Bodies = Matter.Bodies;
- 
+
 var engine;
 var world;
 var boxes = [];
- 
+
 var ground;
 var gSlider;
- 
+
+var gravityInput;
+
+
 function setup() {
     createCanvas(400, 400);
-
-    // Create an instance of Engine, World
     engine = Engine.create();
     world = engine.world;
- 
-    // A slider is already created for you here. This slider will dictate the gravity of the world
+    Engine.run(engine);
+
     gSlider = createSlider(0, 100, 50);
     gSlider.position(40, 365);
-    gSlider.input = map(engine.world.gravity, gSlider.min, gSlider.max, 0, 1);
- 
-    // Create a ground rectangle that would hold all the boxes and add it to the world.
+    gSlider.input = map(engine.world.gravity.y, gSlider.min, gSlider.max, 0, 1);
 
-}
- 
-function mousePressed() {
-    if (mouseY < 350) {
-        // Every time a mouse press occures create a new box.
+    var options = {
+        isStatic: true
     }
+    ground = Bodies.rectangle(200, height - 50, width, 10, options);
+    World.add(world, ground);
 }
- 
+
+
+
 function draw() {
-    // Draw all the elements including the slider that 
+
 
     background(51);
-    // This is the value of your gravity. You can optionally show it to the viewer.
-    var fVal = gSlider.value();
- 
-    // Use a for loop to show all the boxes
-
-}
- 
-
-// You can either create a file for the class Box or build a simple function that creates one box at a time.
-// I have gone for the second option.
-function Box(x, y, w, h, options) {
-
-    // add options such as friction and restitution. Experiment with the values
-    var options = {
-
+    gravityInput = gSlider.value();
+    engine.world.gravity.y = (gravityInput - 50) / 10;
+    for (var i = 0; i < boxes.length; i++) {
+        boxes[i].show();
     }
- 
-    // create your box using the function arguments
-    // x - x-coordinate
-    // y - y-coordinate
-    // w - width of the box
-    // h - height of the box
+    noStroke();
+    fill(170);
+    strokeWeight(4);
+    rectMode(CENTER);
+    rect(ground.position.x, ground.position.y, width, 10);
+    fill(255);
+    textSize(15);
+    text("Gravity " + engine.world.gravity.y, 170, 381);
+    console.log("Gravity: " + engine.world.gravity.y)
+}
 
- 
-
-    // Create a show method which will draw the box every time it is called inside the draw method.
-    // remember to push and pop.
-    this.show = function () {
+function mousePressed() {
+    if (mouseY < 350) {
+        boxes.push(new Box(mouseX, mouseY, random(10, 40), random(10, 40), random(0, 255)));
     }
 }
